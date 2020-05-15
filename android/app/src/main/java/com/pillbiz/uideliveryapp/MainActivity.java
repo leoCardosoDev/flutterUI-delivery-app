6b8076d5bc969aeb081badcd5efd7303869313db
leoCardosoDev <leocardosodev@gmail.com>
2020-05-15 17:54:03 -0300

    First Commit

---
+++ android/app/src/main/java/com/pillbiz/uideliveryapp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.pillbiz.uideliveryapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/data/data.dart
@@ -0,0 +1,136 @@
+import 'package:uideliveryapp/models/food.dart';
+import 'package:uideliveryapp/models/order.dart';
+import 'package:uideliveryapp/models/restaurant.dart';
+import 'package:uideliveryapp/models/user.dart';
+
+// Food
+final _burrito =
+    Food(imageUrl: 'assets/images/burrito.jpg', name: 'Burrito', price: 8.99);
+final _steak =
+    Food(imageUrl: 'assets/images/steak.jpg', name: 'Bife', price: 17.99);
+final _pasta =
+    Food(imageUrl: 'assets/images/pasta.jpg', name: 'Massa', price: 14.99);
+final _ramen =
+    Food(imageUrl: 'assets/images/ramen.jpg', name: 'Ramen', price: 13.99);
+final _pancakes =
+    Food(imageUrl: 'assets/images/pancakes.jpg', name: 'Panquecas', price: 9.99);
+final _burger =
+    Food(imageUrl: 'assets/images/burger.jpg', name: 'Hamburguer', price: 14.99);
+final _pizza =
+    Food(imageUrl: 'assets/images/pizza.jpg', name: 'Pizza', price: 11.99);
+final _salmon = Food(
+    imageUrl: 'assets/images/salmon.jpg', name: 'Salada de salmão', price: 12.99);
+
+// Restaurants
+final _restaurant0 = Restaurant(
+  imageUrl: 'assets/images/restaurant0.jpg',
+  name: 'Restaurante 0',
+  address: 'Rua Bélgica, 123, Barueri, SP',
+  rating: 5,
+  menu: [_burrito, _steak, _pasta, _ramen, _pancakes, _burger, _pizza, _salmon],
+);
+final _restaurant1 = Restaurant(
+  imageUrl: 'assets/images/restaurant1.jpg',
+  name: 'Restaurante 1',
+  address: 'Rua Bélgica, 123, Barueri, SP',
+  rating: 4,
+  menu: [_steak, _pasta, _ramen, _pancakes, _burger, _pizza],
+);
+final _restaurant2 = Restaurant(
+  imageUrl: 'assets/images/restaurant2.jpg',
+  name: 'Restaurante 2',
+  address: 'Rua Bélgica, 123, Barueri, SP',
+  rating: 4,
+  menu: [_steak, _pasta, _pancakes, _burger, _pizza, _salmon],
+);
+final _restaurant3 = Restaurant(
+  imageUrl: 'assets/images/restaurant3.jpg',
+  name: 'Restaurante 3',
+  address: 'Rua Bélgica, 123, Barueri, SP',
+  rating: 2,
+  menu: [_burrito, _steak, _burger, _pizza, _salmon],
+);
+final _restaurant4 = Restaurant(
+  imageUrl: 'assets/images/restaurant4.jpg',
+  name: 'Restaurante 4',
+  address: 'Rua Bélgica, 123, Barueri, SP',
+  rating: 3,
+  menu: [_burrito, _ramen, _pancakes, _salmon],
+);
+
+final List<Restaurant> restaurants = [
+  _restaurant0,
+  _restaurant1,
+  _restaurant2,
+  _restaurant3,
+  _restaurant4,
+];
+
+// User
+final currentUser = User(
+  name: 'Leonardo',
+  orders: [
+    Order(
+      date: 'Nov 10, 2019',
+      food: _steak,
+      restaurant: _restaurant2,
+      quantity: 1,
+    ),
+    Order(
+      date: 'Nov 8, 2019',
+      food: _ramen,
+      restaurant: _restaurant0,
+      quantity: 3,
+    ),
+    Order(
+      date: 'Nov 5, 2019',
+      food: _burrito,
+      restaurant: _restaurant1,
+      quantity: 2,
+    ),
+    Order(
+      date: 'Nov 2, 2019',
+      food: _salmon,
+      restaurant: _restaurant3,
+      quantity: 1,
+    ),
+    Order(
+      date: 'Nov 1, 2019',
+      food: _pancakes,
+      restaurant: _restaurant4,
+      quantity: 1,
+    ),
+  ],
+  cart: [
+    Order(
+      date: 'Nov 11, 2019',
+      food: _burger,
+      restaurant: _restaurant2,
+      quantity: 2,
+    ),
+    Order(
+      date: 'Nov 11, 2019',
+      food: _pasta,
+      restaurant: _restaurant2,
+      quantity: 1,
+    ),
+    Order(
+      date: 'Nov 11, 2019',
+      food: _salmon,
+      restaurant: _restaurant3,
+      quantity: 1,
+    ),
+    Order(
+      date: 'Nov 11, 2019',
+      food: _pancakes,
+      restaurant: _restaurant4,
+      quantity: 3,
+    ),
+    Order(
+      date: 'Nov 11, 2019',
+      food: _burrito,
+      restaurant: _restaurant1,
+      quantity: 2,
+    ),
+  ],
+);
